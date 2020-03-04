5cf06f788dcbd57be57a83ed91c4d6ed96879436
Gergely Padányi-Gulyás <fegyi001@gmail.com>
2020-03-04 14:36:57 +0100

    add missing params to ProjWKT

--- example/wkt-parser-dart_example.dart
+++ example/wkt-parser-dart_example.dart
@@ -3,7 +3,7 @@ import 'package:wkt_parser/wkt_parser.dart' as wkt_parser;
 void main() {
   // WKT CRS string
   var wktText =
-      'PROJCS["HD72 / EOV",GEOGCS["HD72",DATUM["Hungarian_Datum_1972",SPHEROID["GRS 1967",6378160,298.247167427,AUTHORITY["EPSG","7036"]],TOWGS84[52.17,-71.82,-14.9,0,0,0,0],AUTHORITY["EPSG","6237"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4237"]],PROJECTION["Hotine_Oblique_Mercator_Azimuth_Center"],PARAMETER["latitude_of_center",47.14439372222222],PARAMETER["longitude_of_center",19.04857177777778],PARAMETER["azimuth",90],PARAMETER["rectified_grid_angle",90],PARAMETER["scale_factor",0.99993],PARAMETER["false_easting",650000],PARAMETER["false_northing",200000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Y",EAST],AXIS["X",NORTH],AUTHORITY["EPSG","23700"]]';
+      'PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]';
   // Parsed object of type WKT
   var output = wkt_parser.parseWKT(wktText);
   // This returns a JSON encoded string of the WKT
--- lib/src/proj_wkt.dart
+++ lib/src/proj_wkt.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 
 class ProjWKT {
   // All parameters (also without getters)
-  Map<dynamic, dynamic> map;
+  Map<dynamic, dynamic> map = {};
 
   @override
   String toString() => jsonEncode(map);
@@ -14,12 +14,14 @@ class ProjWKT {
   double get latitude_of_center => map['latitude_of_center'];
   double get longitude_of_center => map['longitude_of_center'];
   double get azimuth => map['azimuth'];
+  double get central_meridian => map['central_meridian'];
   double get rectified_grid_angle => map['rectified_grid_angle'];
   double get scale_factor => map['scale_factor'];
   double get false_easting => map['false_easting'];
   double get false_northing => map['false_northing'];
   Map<String, dynamic> get UNIT => map['UNIT'];
   List<dynamic> get AXIS => map['AXIS'];
+  Map<dynamic, dynamic> get EXTENSION => map['EXTENSION'];
   Map<dynamic, dynamic> get AUTHORITY => map['AUTHORITY'];
   String get projName => map['projName'];
   String get units => map['units'];
@@ -31,11 +33,16 @@ class ProjWKT {
   List<dynamic> get datum_params => map['datum_params'];
   double get k0 => map['k0'];
   double get lat0 => map['lat0'];
+  double get long0 => map['long0'];
   double get longc => map['longc'];
   double get x0 => map['x0'];
   double get y0 => map['y0'];
   double get alpha => map['alpha'];
   String get srsCode => map['srsCode'];
 
-  ProjWKT(Map<dynamic, dynamic> parsedWkt) : map = parsedWkt;
+  ProjWKT(Map<dynamic, dynamic> parsedWkt) {
+    parsedWkt.forEach((key, value) {
+      map[key] = value;
+    });
+  }
 }
